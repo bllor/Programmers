4e235b12f87f4585b6a9b3a2ddcf10fee173e5a7
bllor <duki0105@gmail.com>
2023-11-22 14:55:42 +0900

    2문제 풀이

---
+++ Lv0/src/test1.java
@@ -0,0 +1,14 @@
+
+public class test1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		// 알파벳을 소문자로 출력하기
+			String newArgs = "AString";
+			String answer = newArgs.toLowerCase();
+			System.out.println(answer);
+		//대문자로 출력하기
+			answer = newArgs.toUpperCase();
+	}
+
+}
