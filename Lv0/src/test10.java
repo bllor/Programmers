141ee7a46873290fb9757d425eaeb84f68210595
bllor <duki0105@gmail.com>
2023-11-25 14:00:50 +0900

    test10까지 진행

---
+++ Lv0/src/test10.java
@@ -0,0 +1,12 @@
+
+public class test10 {
+	public static void main() {
+		int num = 98;
+		int num2 = 2;
+		int answer = 0;
+		if(num/num2==0) {
+			answer = 1;
+		}
+		System.out.println("answer : "+answer);
+	}
+}
