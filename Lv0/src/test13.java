2a1c1db40b3c6d07dfe8c0010d2bdf48742f261f
bllor <duki0105@gmail.com>
2023-11-26 10:58:09 +0900

    test15

---
+++ Lv0/src/test13.java
@@ -0,0 +1,17 @@
+
+public class test13 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		// 문자열 곱하기
+		
+		String s = "love";
+		int k  = 3;
+		String answer = "";
+		for(int i =0;i<k;i++) {
+			answer+=s;
+		}
+		System.out.println("answer : "+answer);
+	}
+
+}
