1bf5a0b142a4609102e7f558d9ba55db31ab2e90
bllor <duki0105@gmail.com>
2024-04-08 07:21:31 +0900

    test83진행중

--- Lv0/src/test81_100/test83.java
+++ Lv0/src/test81_100/test83.java
@@ -4,12 +4,23 @@ public class test83 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		// 특정 문자열로 끝나는 가장 긴 부분 문자열 찾기
-		String myString = "AAAAaaaa";
-		String pat = "a";
+		// 배열 만들기 (5)
 		
-		int a = myString.lastIndexOf(pat);
-		System.out.println(myString.substring(0, a)+pat);
+	String intStre[] = {"0123456789","9876543210","9999999999999"};
+	int k = 50000;
+	int s = 5;
+	int l = 5;
+		
+	}
+	
+	public int[] solution(String[] intStrs, int k , int s, int l ) {
+		int[] answer = {};
+		for(int i = 0; i<intStrs.length ;i++) {
+		System.out.println(intStrs[i]);
+		}
+		
+		
+		return answer;
 	}
 
 }
